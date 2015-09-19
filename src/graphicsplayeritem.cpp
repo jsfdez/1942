5e8efa9503862f07958f078b09efcddb7c80f15b
Jesus Fernandez <jsfdez@gmail.com>
2015-09-19 17:46:01 +0200

    fixed shadow

--- src/graphicsplayeritem.cpp
+++ src/graphicsplayeritem.cpp
@@ -4,24 +4,23 @@
 #include <QPainter>
 #include <QKeyEvent>
 #include <QGraphicsScene>
+#include <QGraphicsDropShadowEffect>
 
 #include "pixmapcache.h"
 
 GraphicsPlayerItem::GraphicsPlayerItem(QGraphicsItem *parent)
     : QGraphicsItem(parent)
 {
+    auto effect = new QGraphicsDropShadowEffect;
+    effect->setColor(Qt::black);
+    setGraphicsEffect(effect);
     setFlag(GraphicsPlayerItem::ItemIsFocusable, true);
 }
 
 QRectF GraphicsPlayerItem::boundingRect() const
 {
-    const auto player = PixmapCache::player();
-    QRectF rect{
-        -player.rect().width() / 8.0f,
-        -player.rect().height() / 2.0f,
-        player.rect().width() / 4.0f,
-        static_cast<qreal>(player.rect().height()),
-    };
+    auto rect = PixmapCache::player().rect();
+    rect.setWidth(rect.width() / 4);
     return rect;
 }
 
@@ -36,31 +35,37 @@ void GraphicsPlayerItem::paint(QPainter *painter,
 
 void GraphicsPlayerItem::advance(int phase)
 {
+    auto p = pos();
+    const auto sceneRect = scene()->sceneRect();
+
+    // Horizontal movement
     if (m_keys.contains(Qt::Key_Left) && m_keys.contains(Qt::Key_Right));
     else if (m_keys.contains(Qt::Key_Left))
-        setPos(qMax(scene()->sceneRect().x(), pos().x() - k_speed), pos().y());
+        p.setX(qMax(p.x() - k_speed, sceneRect.x()));
     else if (m_keys.contains(Qt::Key_Right))
     {
-        setPos(qMin(scene()->sceneRect().width() - boundingRect().width(),
-            pos().x() + k_speed), pos().y());
+        const auto right = sceneRect.x() + sceneRect.width()
+            - boundingRect().width();
+        p.setX(qMin(p.x() + k_speed, right));
     }
 
+    // Vertical movement
     if (m_keys.contains(Qt::Key_Up) && m_keys.contains(Qt::Key_Down));
     else if (m_keys.contains(Qt::Key_Up))
-        setPos(pos().x(), qMax(scene()->sceneRect().x(), pos().y() - k_speed));
+        p.setY(qMax(p.y() - k_speed, sceneRect.y()));
     else if (m_keys.contains(Qt::Key_Down))
     {
-        setPos(pos().x(),
-            qMin(scene()->sceneRect().height() - boundingRect().height(),
-            pos().y() + k_speed));
+        const auto bottom = sceneRect.y() + sceneRect.height()
+            - boundingRect().height();
+        p.setY(qMin(p.y() + k_speed, bottom));
     }
 
+    setPos(p);
     m_frame = phase % 3;
 }
 
 void GraphicsPlayerItem::keyPressEvent(QKeyEvent *event)
 {
-    qDebug() << "Pressed key:" << event->key() << Qt::LeftArrow;
     switch(event->key())
     {
     case Qt::Key_Left:
@@ -68,6 +73,9 @@ void GraphicsPlayerItem::keyPressEvent(QKeyEvent *event)
     case Qt::Key_Up:
     case Qt::Key_Down:
         m_keys.insert(event->key());
+        break;
+    case Qt::Key_S:
+        graphicsEffect()->setEnabled(!graphicsEffect()->isEnabled());
         event->accept();
         break;
     default:
